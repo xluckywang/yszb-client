eb2cc01648f22e66f4107f4013c64d017bd0c66a
Shine_AL <635597602@qq.com>
2020-11-03 10:13:35 +0800

    time to set compelet

--- src/views/DeviceManage/CheckSet.vue
+++ src/views/DeviceManage/CheckSet.vue
@@ -14,7 +14,7 @@
         <el-col :span="10" style="text-align : right;">
           <!-- <el-button type="primary" size="small" @click="defaultSet()">恢复默认</el-button> -->
           <el-button type="primary" size="small" @click="saveDialogVisible=true,gridData = multipleSelection">保存</el-button>
-          <el-button type="primary" size="small" @click="handleBack()">取消</el-button>
+          <el-button type="primary" size="small" @click="$router.go(-1)">取消</el-button>
         </el-col>
       </el-row>
     </div>
@@ -37,72 +37,65 @@
     <el-row :gutter="20">
       <el-col :span="6">
         <div class="container">
-          <el-button type="primary" size="small" @click="setDialogVisible = true">设置日点检计划</el-button>
+          <el-button type="primary" size="small" @click="handleSetDialog('d')">设置日点检计划</el-button>
           <el-table :data="dList" style="width: 100%" height="600">
             <el-table-column type="index" label="序号"></el-table-column>
-            <el-table-column prop="date" label="项目名称" min-width="100"></el-table-column>
+            <el-table-column prop="djxmName" label="项目名称" min-width="100"></el-table-column>
           </el-table>
         </div>
       </el-col>
       <el-col :span="6">
         <div class="container">
-          <el-button type="primary" size="small">设置周点检计划</el-button>
+          <el-button type="primary" size="small" @click="handleSetDialog('w')">设置周点检计划</el-button>
           <el-table :data="wList" style="width: 100%" height="600">
             <el-table-column type="index" label="序号"></el-table-column>
-            <el-table-column prop="date" label="项目名称" min-width="100"></el-table-column>
+            <el-table-column prop="djxmName" label="项目名称" min-width="100"></el-table-column>
           </el-table>
         </div>
       </el-col>
       <el-col :span="6">
         <div class="container">
-          <el-button type="primary" size="small">设置月点检计划</el-button>
+          <el-button type="primary" size="small" @click="handleSetDialog('m')">设置月点检计划</el-button>
           <el-table :data="mList" style="width: 100%" height="600">
             <el-table-column type="index" label="序号"></el-table-column>
-            <el-table-column prop="date" label="项目名称" min-width="100"></el-table-column>
+            <el-table-column prop="djxmName" label="项目名称" min-width="100"></el-table-column>
           </el-table>
         </div>
       </el-col>
       <el-col :span="6">
         <div class="container">
-          <el-button type="primary" size="small">设置年点检计划</el-button>
+          <el-button type="primary" size="small" @click="handleSetDialog('y')">设置年点检计划</el-button>
           <el-table :data="yList" style="width: 100%" height="600">
             <el-table-column type="index" label="序号"></el-table-column>
-            <el-table-column prop="date" label="项目名称" min-width="100"></el-table-column>
+            <el-table-column prop="djxmName" label="项目名称" min-width="100"></el-table-column>
           </el-table>
         </div>
       </el-col>
     </el-row>
     <!-- 设置点检计划弹窗 -->
     <el-dialog title="选择点检项目" :visible.sync="setDialogVisible">
-      <el-table ref="multipleTable" :data="djxmList" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+      <el-table ref="multipleTable" :data="djxmList" tooltip-effect="dark" max-height="300" style="width: 100%"
+        @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="55"></el-table-column>
-        <el-table-column prop="name" label="项目名称" width="120"></el-table-column>
-        <el-table-column prop="address" label="项目内容"></el-table-column>
+        <el-table-column prop="djxmName" label="项目名称" width="200"></el-table-column>
+        <el-table-column prop="djxmContent" label="项目内容"></el-table-column>
       </el-table>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="saveDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="saveSet">确 定</el-button>
+        <el-button @click="setDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="setConfirm">确 定</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { getDjxmList, addDjfa, getDefaultConfig } from '@/api/DeviceManage/CheckSet';
+import { addDjfa } from '@/api/DeviceManage/CheckSet';
+import { getDjxmList } from '@/api/DeviceManage/CheckManage';
 
 export default {
   data () {
     return {
-      tableData: [
-        {
-          djxmId: 'S01',
-          djlxId: '开机点检',
-          djxmName: '油液位标尺',
-          djxmDetail: '点检项目内容'
-        }
-      ],
       gridData: [],
-      search: '',
       multipleSelection: [],
       saveDialogVisible: false,
 
@@ -121,6 +114,20 @@ export default {
   mounted () {
 
   },
+  updated () {
+    switch (this.period) {
+      case 'd':
+        this.toggleSelection(this.dList); break;
+      case 'w':
+        this.toggleSelection(this.wList); break;
+      case 'm':
+        this.toggleSelection(this.mList); break;
+      case 'y':
+        this.toggleSelection(this.yList); break;
+      default:
+        this.toggleSelection(); break;
+    }
+  },
   methods: {
     init () {
       getDjxmList('').then(res => {
@@ -133,13 +140,35 @@ export default {
           position: 'top',
           time: 2000
         })
-      })
+      });
+      getDjxmList().then(res => {
+        // console.log(res.data.data);
+        this.djxmList = res.data.data;
+      });
+    },
+    handleSetDialog (period) {
+      this.setDialogVisible = true;
+      this.period = period;
+    },
+    setConfirm () {
+      this.setDialogVisible = false;
+      switch (this.period) {
+        case 'd':
+          this.dList = this.multipleSelection; break;
+        case 'w':
+          this.wList = this.multipleSelection; break;
+        case 'm':
+          this.mList = this.multipleSelection; break;
+        case 'y':
+          this.yList = this.multipleSelection; break;
+      }
+      console.log(this.dList);
     },
     // 保存
     saveSet () {
       this.format(this.gridData)
       this.handleAdd(this.gridData);
-      this.$router.go(-1);
+      // this.$router.go(-1);
     },
 
     // 添加点检方案请求数据整理
@@ -172,42 +201,14 @@ export default {
         rows.forEach(row => {
           this.$refs.multipleTable.toggleRowSelection(row);
         });
-      } else {
-        this.$refs.multipleTable.clearSelection();
       }
+      // else {
+      //   this.$refs.multipleTable.clearSelection();
+      // }
     },
     handleSelectionChange (val) {
+      // console.log(val)
       this.multipleSelection = val;
-    },
-
-    // 返回
-    handleBack () {
-      this.$router.go(-1);
-    },
-
-    // 恢复默认点检设置
-    defaultSet () {
-      // console.log('default')
-      getDefaultConfig(this.$router.history.current.params.id).then(res => {
-        // console.log(res.data)
-        if (res.data.code == 'SUCCESS') {
-          // console.log(res.data.data)
-          this.$confirm('确定要恢复成默认点检方案？', '提示', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'warning'
-          }).then(() => {
-            this.handleAdd(res.data.data);
-            this.$router.go(-1);
-          })
-        } else {
-          this.$message.error({
-            message: '该设备暂无默认点检方案',
-            position: 'top',
-            time: 2000
-          })
-        }
-      })
     }
   }
 };
